e6945ebea693c9268f6f1770cf67c989d9985a64
Areeba Kausar <areebakausar@ymail.com>
2019-11-20 05:10:50 -0500

    CheckPoint1

---
+++ finalGameProject/game.py
@@ -0,0 +1,41 @@
+#Areeba Kausar (ak3rej) and Faiz Khan (fk6jr)
+
+# Game Idea: Multiplayer Race taking place
+
+# Required Features:
+
+# User Input: Two players will be laying the game, one will use w and s keys,
+# other will use and up and down arrow. Game will start with pressing of spacebar
+
+# Graphics/Images spritesheet of two characters will be inserted who will be running
+# through the game
+
+# Start Screen will be built using camera. It will disappear once spacebar is hit
+
+# window size: gamebox.Camera(600, 400) will be implemented
+
+# Optional Features:
+
+#Two players: Move through an obstacle course, starting from opposite sides of the
+# screen and working to reach the middle first
+
+#Collectibles
+#Power- ups are spread around the course, and users collect them to help them win
+#Super speed→ allows player to speed through the obstacle course
+# Teleportation→ if you collect this power-up you mover to a further point in the game
+# Time saver→ takes 3 or 4 seconds off the players timer
+
+#Timer: In the corner, counts each of the players individual times
+# Determines winner in the end
+
+# Health bar:
+# If player hits the boundary, they lose a life
+# Boundaries are spaced close at some points and farther at others making it more
+# difficult to navigate through the maze
+
+#Save points
+# Can run multiple races where best out of 3 races wins the tournament
+
+
+
+
